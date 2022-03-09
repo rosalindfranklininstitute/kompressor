275c8db66d8188a20de74ba9bcc231dee8e41bb9
= <=>
2022-03-09 11:33:48 +0000

    Creating Plugin Method for Models
    So that different Models can be selected and used for training a plugin method is being implemented.

---
+++ training/models/__init__.py
@@ -0,0 +1,29 @@
+"""Helper functions to store and get the selected model"""
+from collections import namedtuple
+from training.utils import named_module
+
+_current = None
+AVAILABLE = ['cnn', 'mlp']
+Model = namedtuple('Model', ['name', "Model"])
+
+def use_model(model):
+    model = model.lower()
+    global _current
+    if model in AVAILABLE:
+        module_ = named_module('training.models.{}'.format(model))
+        if hasattr(module_, '_model'):
+            _current = module_._model
+        else:
+            raise Exception(
+                'Module `{}` is not a proper model.'.format(model))
+    else:
+        raise Exception('Model `{}` not available! Choose from: {}'
+                        .format(model, AVAILABLE))
+
+
+def get_model(name=None):
+    if name is not None:
+        use_model(name)
+    if _current is None:
+        use_model(AVAILABLE[0])
+    return _current
