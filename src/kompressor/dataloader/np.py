df738b17d234c6f2ad504a30c32039d23bd18042
= <gabryelmw@gmail.com>
2024-07-10 15:36:37 +0100

    Moved dataloader from notebook to own module.

---
+++ src/kompressor/dataloader/np.py
@@ -0,0 +1,37 @@
+import numpy as np
+from jax import tree_map
+from torch.utils import data
+from torch.utils.data import DataLoader
+
+
+def numpy_collate(batch):
+    return tree_map(np.asarray, data.default_collate(batch))
+
+
+class NumpyLoader(DataLoader):
+    def __init__(
+        self,
+        dataset,
+        batch_size=1,
+        shuffle=False,
+        sampler=None,
+        batch_sampler=None,
+        num_workers=0,
+        pin_memory=False,
+        drop_last=False,
+        timeout=0,
+        worker_init_fn=None,
+    ):
+        super(self.__class__, self).__init__(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            sampler=sampler,
+            batch_sampler=batch_sampler,
+            num_workers=num_workers,
+            collate_fn=numpy_collate,
+            pin_memory=pin_memory,
+            drop_last=drop_last,
+            timeout=timeout,
+            worker_init_fn=worker_init_fn,
+        )
